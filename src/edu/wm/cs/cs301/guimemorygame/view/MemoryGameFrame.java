d78fd9110e2bc7c2f2a9871827fcbafa50b26c93
Peyton Boggs <peytonrva@gmail.com>
2023-12-17 12:42:25 -0500

    Instructions Frame
    This commit adds the Instructions frame when the game is first started
    thought the displayInstuctions method. This method packs a frame with
    all of the text instructions for the game, and sets it visible for the
    user. This method is only called once when the program is first run.

--- src/edu/wm/cs/cs301/guimemorygame/view/MemoryGameFrame.java
+++ src/edu/wm/cs/cs301/guimemorygame/view/MemoryGameFrame.java
@@ -27,8 +27,30 @@ public class MemoryGameFrame {
 	private final JPanel characterGridPanel;
 	
 	public MemoryGameFrame(MemoryGameModel model) {
+		displayInstructions();
+		
 		this.characterGridPanel = createCharacterGridPanel(model.getRows(), model.getCols());
 		this.frame = createAndShowGUI();
+	}
+	
+	private void displayInstructions() {
+		JFrame frame = new JFrame("Instructions");
+		frame.setResizable(false);
+		
+		JPanel panel = new JPanel(new GridLayout(6, 1));
+		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		
+		panel.add(new JLabel("Welcome to Memory Game!"));
+		panel.add(new JLabel());
+		panel.add(new JLabel("In this game, you'll be shown a grid of cards that are initially flipped over."));
+		panel.add(new JLabel("Each turn, you can select two cards to be flipped over by clicking on them."));
+		panel.add(new JLabel("If the two cards match, they will stay revealed - if not, they will be flipped back over."));
+		panel.add(new JLabel("The object is to reveal all cards in as few turns as possible. Good luck!"));
+		
+		frame.add(panel);
+		
+		frame.pack();
+		frame.setVisible(true);		
 	}
 	
 	private JFrame createAndShowGUI() {
