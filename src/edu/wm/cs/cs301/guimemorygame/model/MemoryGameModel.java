083f131c9c446eae59468caa374bdc669ca994d3
Peyton Boggs <peytonrva@gmail.com>
2023-12-17 15:10:14 -0500

    Truncating + Shuffling Characters
    This commit adds logic to the generatePieces() method that puts only 6
    shuffled CharacterGamePiece pairs into the array, so that the proper
    pieces are in the final board. The pieces have also been temporarily set
    to visible for the purposes of testing.

--- src/edu/wm/cs/cs301/guimemorygame/model/MemoryGameModel.java
+++ src/edu/wm/cs/cs301/guimemorygame/model/MemoryGameModel.java
@@ -1,5 +1,9 @@
 package edu.wm.cs.cs301.guimemorygame.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import edu.wm.cs.cs301.guimemorygame.view.CharacterGamePiece;
 
 public class MemoryGameModel {
@@ -17,13 +21,27 @@ public class MemoryGameModel {
 	private CharacterGamePiece[][] generatePieces() {
 		CharacterGamePiece[][] board = new CharacterGamePiece[this.rows][this.cols];
 		
-		char[] symbols = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 
+		char[] tempSymbols = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 
 		'b'};
+		
+		char[] alphabet = new char[6];
+		System.arraycopy(tempSymbols, 0, alphabet, 0, 6);
+		
+		List<Character> symbolList = new ArrayList<Character> ();
+		for (char c : alphabet) {
+			symbolList.add(c);
+		}
+		
+		symbolList.addAll(symbolList);
+		Collections.shuffle(symbolList);
+		
+		
 		
 		int symbolIndex = 0;
 		for (int i = 0; i < this.rows; i++) {
 			for (int j = 0; j < this.cols; j++) {
-				CharacterGamePiece temp = new CharacterGamePiece(symbols[symbolIndex]);
+				CharacterGamePiece temp = new CharacterGamePiece(symbolList.get(symbolIndex));
+				temp.setVisible(true);
 				symbolIndex++;
 				board[i][j] = temp;
 			}
